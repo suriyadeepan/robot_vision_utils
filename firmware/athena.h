4ca006d78148e4739b4519c50cd815a39315fe39
vanangamudi <selva.developer@gmail.com>
2012-10-29 16:42:29 +0530

    separated some of the functionalities into separate files... more to do.

---
+++ firmware/athena.h
@@ -0,0 +1,78 @@
+/****************************** 
+ * Program : Firmware for uC  *
+ * Author  : Selvakumar       *
+ * Date    : 28/10/2012       * 
+ ******************************/
+
+
+#ifndef _ATHENA_H_
+#define _ATHENA_H_
+
+
+/* compiled for testing in pc. so needed to be commented
+
+#include<avr/io.h>
+#include<util/dealy.h>
+
+*/
+
+#include<stdio.h>
+
+#define LOCO_CONT_BEGIN 00                               /* 00 */
+#define LOCO_CONT_LEN   10
+#define LOCO_CONT_END   LOCO_CONT_BEGIN + LOCO_CONT_LEN  /* 10 */
+
+#define CAM_CONT_BEGIN  LOCO_CONT_END                    /* 10 */
+#define CAM_CONT_LEN    10
+#define CAM_CONT_END    CAM_CONT_BEGIN + CAM_CONT_LEN    /* 20 */
+
+#define ARM_CONT_BEGIN  CAM_CONT_END                     /* 20 */
+#define ARM_CONT_LEN    10
+#define ARM_CONT_END    ARM_CONT_BEGIN + ARM_CONT_LEN    /* 30 */
+
+/************************************************************
+ * Actual operations                                        *
+ * they are used as indices for                             *
+ * <category>_<opertion>_<dirction> function pointer array  *
+ ************************************************************/
+
+/*  LOCOMOTION OPERATIONS */
+#define LOCO_FORWARD      LOCO_CONT_BEGIN +  01
+#define LOCO_BACKWARD     LOCO_CONT_BEGIN +  02
+#define LOCO_TURN_RIGHT   LOCO_CONT_BEGIN +  03
+#define LOCO_TURN_LEFT    LOCO_CONT_BEGIN +  04
+#define LOCO_TURN_AROUND  LOCO_CONT_BEGIN +  05
+/*  CAMERA HANDLING */
+#define CAM_LOOK_UP       CAM_CONT_BEGIN + 01
+#define CAM_LOOK_DOWN     CAM_CONT_BEGIN + 02
+#define CAM_LOOK_RIGHT    CAM_CONT_BEGIN + 03
+#define CAM_LOOK_LEFT     CAM_CONT_BEGIN + 04
+/* ARM CONTROL */
+#define ARM_CONTRACT      ARM_CONT_BEGIN + 01
+#define ARM_DILATE        ARM_CONT_BEGIN + 02
+#define ARM_UP            ARM_CONT_BEGIN + 03
+#define ARM_DOWN          ARM_CONT_BEGIN + 04
+
+/* function prototypes with similar signatures atleast for each category */
+/*locomotion - category */
+int loco_forward();
+int loco_backward();
+int loco_turn_right();
+int loco_turn_left();
+int loco_turn_around();
+
+/*camera - category */
+int cam_look_up();
+int cam_look_down();
+int cam_look_right();
+int cam_look_left();
+
+/*arm - category */
+int arm_contract();
+int arm_dilate();
+int arm_up();
+int arm_down();
+
+int USART_receive();
+
+#endif /* _ATHENA_H_ */
--- firmware/main.c
+++ firmware/main.c
@@ -3,79 +3,7 @@
  * Author  : Selvakumar       *
  * Date    : 28/10/2012       * 
  ******************************/
-
-/* compiled for testing in pc. so needed to be commented
-
-#include<avr/io.h>
-#include<util/dealy.h>
-
-*/
-
-#include<stdio.h>
-
-/*  LOCOMOTION OPERATIONS */
-#define LOCO_FORWARD      01
-#define LOCO_BACKWARD     02
-#define LOCO_TURN_RIGHT   03
-#define LOCO_TURN_LEFT    04
-#define LOCO_TURN_AROUND  05
-/*  CAMERA HANDLING */
-#define CAM_LOOK_UP       11
-#define CAM_LOOK_DOWN     12
-#define CAM_LOOK_RIGHT    13
-#define CAM_LOOK_LEFT     14
-/* ARM CONTROL */
-#define ARM_CONTRACT      21
-#define ARM_DILATE        22
-#define ARM_UP            23
-#define ARM_DOWN          24
-
-/* function prototypes with similar signatures atleast for each category */
-/*locomotion - category */
-int loco_forward();
-int loco_backward();
-int loco_turn_right();
-int loco_turn_left();
-int loco_turn_around();
-
-/*camera - category */
-int cam_look_up();
-int cam_look_down();
-int cam_look_right();
-int cam_look_left();
-
-/*arm - category */
-int arm_contract();
-int arm_dilate();
-int arm_up();
-int arm_down();
-
-/* array of function pointers        *
- * datatype (*fp[])(argument list);  *
- */
-int (*loco_control[])() = { loco_forward,
-			    loco_backward,
-			    loco_turn_right,
-			    loco_turn_left,
-			    loco_turn_around 
-                          };
-
-
-int (*arm_control[])() = {  arm_contract,
-			    arm_dilate,
-			    arm_up,
-			    arm_down
-                          };
-
-
-int (*cam_control[])() = { cam_look_up,
-			    cam_look_down,
-			    cam_look_left,
-			    cam_look_right
-                          };
-
-
-/* other utility functions */
+#include "athena.h"
 
 int USART_receive()
 {
@@ -84,6 +12,32 @@ int USART_receive()
   scanf("%d",&num);
   return num;
 }
+
+/* array of function pointers        *
+ * datatype (*fp[])(argument list);  *
+ */
+/* TODO: will be moved to athena.c soon */
+int (*loco_control[])() = {  loco_forward,
+			     loco_backward,
+			     loco_turn_right,
+			     loco_turn_left,
+			     loco_turn_around 
+                          };
+
+
+int (*arm_control[])() = {  arm_contract, 
+			    arm_dilate,
+			    arm_up,
+			    arm_down
+                          };
+
+
+int (*cam_control[])() = {  cam_look_up,
+			    cam_look_down,
+			    cam_look_left,
+			    cam_look_right
+                          };
+
 
 int main()
 {
@@ -97,18 +51,18 @@ int main()
     command = USART_receive();
     if(command > 0){
       
-      if(command <= 10){
-	command = command - 1;
+      if(command <= LOCO_CONT_END){
+	command = command - (LOCO_CONT_BEGIN+1);
 	loco_control[command]();
 	continue;
       }
-      else if (command <= 20){
-	command = command - 11;
+      else if (command <= CAM_CONT_END){
+	command = command - (CAM_CONT_BEGIN+1);
 	cam_control[command]();
 	continue;
       }
-      else if(command <= 30){
-	command = command -21;
+      else if(command <= ARM_CONT_END){
+	command = command - (ARM_CONT_BEGIN+1);
 	arm_control[command]();
 	continue;
       }
@@ -119,82 +73,3 @@ int main()
 
 }
 
-
-int loco_forward()
-{
-  printf("\n %d",LOCO_FORWARD);
-  
-}
-
-int loco_backward()
-{
-  printf("\n %d",LOCO_BACKWARD);
-  
-}
-
-int loco_turn_right()
-{
-  printf("\n %d",LOCO_TURN_RIGHT);
-  
-}
-
-int loco_turn_left()
-{
-  printf("\n %d",LOCO_TURN_LEFT);
-  
-}
-
-int loco_turn_around()
-{
-  printf("\n %d",LOCO_TURN_AROUND);
-  
-}
-
-int cam_look_up()
-{
-  printf("\n %d",CAM_LOOK_UP);
-  
-}
-
-int cam_look_down()
-{
-  printf("\n %d",CAM_LOOK_DOWN);
-  
-}
-
-int cam_look_right()
-{
-  printf("\n %d",CAM_LOOK_RIGHT);
-  
-}
-
-int cam_look_left()
-{
-  printf("\n %d",CAM_LOOK_LEFT);
-  
-}
-
-int arm_contract()
-{
-  printf("\n %d",ARM_CONTRACT);
-  
-
-}
-
-int arm_dilate()
-{
-  printf("\n %d",ARM_DILATE);
-  
-}
-
-int arm_up()
-{
-  printf("\n %d",ARM_UP);
-  
-}
-
-int arm_down()
-{
-  printf("\n %d",ARM_DOWN);
-  
-}
