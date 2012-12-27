59a7ecec03ea9c90c2e6a48a7ec7bb02b2de6527
rps-laptop <developer.rps@gmail.com>
2012-12-27 14:15:02 +0530

    I am tired

--- firmware/main.c
+++ firmware/main.c
@@ -3,54 +3,7 @@
  * Author  : Selvakumar       *
  * Date    : 28/10/2012       * 
  ******************************/
-<<<<<<< HEAD
 
-/* compiled for testing in pc. so needed to be commented
-
-#include<avr/io.h>
-#include<util/dealy.h>
-
-*/
-
-#include<stdio.h>
-
-/*  LOCOMOTION OPERATIONS */
-#define LOCO_FORWARD      01
-#define LOCO_BACKWARD     02
-#define LOCO_TURN_RIGHT   03
-#define LOCO_TURN_LEFT    04
-#define LOCO_TURN_AROUND  05
-/*  CAMERA HANDLING */
-#define CAM_LOOK_UP       11
-#define CAM_LOOK_DOWN     12
-#define CAM_LOOK_RIGHT    13
-#define CAM_LOOK_LEFT     14
-/* ARM CONTROL */
-#define ARM_CONTRACT      21
-#define ARM_DILATE        22
-#define ARM_UP            23
-#define ARM_DOWN          24
-
-/* function prototypes with similar signatures atleast for each category */
-/*locomotion - category */
-int loco_forward();
-int loco_backward();
-int loco_turn_right();
-int loco_turn_left();
-int loco_turn_around();
-
-/*camera - category */
-int cam_look_up();
-int cam_look_down();
-int cam_look_right();
-int cam_look_left();
-
-/*arm - category */
-int arm_contract();
-int arm_dilate();
-int arm_up();
-int arm_down();
-=======
 #include "athena.h"
 
 int USART_receive()
@@ -60,22 +13,11 @@ int USART_receive()
   scanf("%d",&num);
   return num;
 }
->>>>>>> 4ca006d78148e4739b4519c50cd815a39315fe39
 
 /* array of function pointers        *
  * datatype (*fp[])(argument list);  *
  */
-<<<<<<< HEAD
-int (*loco_control[])() = { loco_forward,
-			    loco_backward,
-			    loco_turn_right,
-			    loco_turn_left,
-			    loco_turn_around 
-                          };
 
-
-int (*arm_control[])() = {  arm_contract,
-=======
 /* TODO: will be moved to athena.c soon */
 int (*loco_control[])() = {  loco_forward,
 			     loco_backward,
@@ -86,37 +28,20 @@ int (*loco_control[])() = {  loco_forward,
 
 
 int (*arm_control[])() = {  arm_contract, 
->>>>>>> 4ca006d78148e4739b4519c50cd815a39315fe39
 			    arm_dilate,
 			    arm_up,
 			    arm_down
                           };
 
 
-<<<<<<< HEAD
-int (*cam_control[])() = { cam_look_up,
-=======
+
 int (*cam_control[])() = {  cam_look_up,
->>>>>>> 4ca006d78148e4739b4519c50cd815a39315fe39
 			    cam_look_down,
 			    cam_look_left,
 			    cam_look_right
                           };
 
 
-<<<<<<< HEAD
-/* other utility functions */
-
-int USART_receive()
-{
-  int num;
-  printf("\nEnter a number ");
-  scanf("%d",&num);
-  return num;
-}
-
-=======
->>>>>>> 4ca006d78148e4739b4519c50cd815a39315fe39
 int main()
 {
   int command;
@@ -129,20 +54,7 @@ int main()
     command = USART_receive();
     if(command > 0){
       
-<<<<<<< HEAD
-      if(command <= 10){
-	command = command - 1;
-	loco_control[command]();
-	continue;
-      }
-      else if (command <= 20){
-	command = command - 11;
-	cam_control[command]();
-	continue;
-      }
-      else if(command <= 30){
-	command = command -21;
-=======
+
       if(command <= LOCO_CONT_END){
 	command = command - (LOCO_CONT_BEGIN+1);
 	loco_control[command]();
@@ -155,7 +67,6 @@ int main()
       }
       else if(command <= ARM_CONT_END){
 	command = command - (ARM_CONT_BEGIN+1);
->>>>>>> 4ca006d78148e4739b4519c50cd815a39315fe39
 	arm_control[command]();
 	continue;
       }
@@ -166,85 +77,3 @@ int main()
 
 }
 
-<<<<<<< HEAD
-
-int loco_forward()
-{
-  printf("\n %d",LOCO_FORWARD);
-  
-}
-
-int loco_backward()
-{
-  printf("\n %d",LOCO_BACKWARD);
-  
-}
-
-int loco_turn_right()
-{
-  printf("\n %d",LOCO_TURN_RIGHT);
-  
-}
-
-int loco_turn_left()
-{
-  printf("\n %d",LOCO_TURN_LEFT);
-  
-}
-
-int loco_turn_around()
-{
-  printf("\n %d",LOCO_TURN_AROUND);
-  
-}
-
-int cam_look_up()
-{
-  printf("\n %d",CAM_LOOK_UP);
-  
-}
-
-int cam_look_down()
-{
-  printf("\n %d",CAM_LOOK_DOWN);
-  
-}
-
-int cam_look_right()
-{
-  printf("\n %d",CAM_LOOK_RIGHT);
-  
-}
-
-int cam_look_left()
-{
-  printf("\n %d",CAM_LOOK_LEFT);
-  
-}
-
-int arm_contract()
-{
-  printf("\n %d",ARM_CONTRACT);
-  
-
-}
-
-int arm_dilate()
-{
-  printf("\n %d",ARM_DILATE);
-  
-}
-
-int arm_up()
-{
-  printf("\n %d",ARM_UP);
-  
-}
-
-int arm_down()
-{
-  printf("\n %d",ARM_DOWN);
-  
-}
-=======
->>>>>>> 4ca006d78148e4739b4519c50cd815a39315fe39
